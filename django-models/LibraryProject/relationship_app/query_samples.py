0c6a1c155eb8f6560aa26ddb6d1e12ad82207d8f
Alazar Zemene <149241148+alazem@users.noreply.github.com>
2025-02-27 01:44:50 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app.models import Book, Author, Library, Librarian
 
 def book_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)  # Changed to use .filter()
+    books = Book.objects.filter(author=author)  # Ensures filter() is used
     for book in books:
         print(f"Title: {book.title} by Author: {author.name}")
 
@@ -14,9 +14,8 @@ def books_in_library(library_name):
 
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.filter(library=library)  # Using filter() for retrieval
-    for person in librarian:
-        print(f"Librarian: {person.name}, Library: {library.name}")
+    librarian = Librarian.objects.get(library=library)  # Changed filter() to get()
+    print(f"Librarian: {librarian.name}, Library: {library.name}")
 
 if __name__ == '__main__':
     book_by_author('J.K. Rowling')
