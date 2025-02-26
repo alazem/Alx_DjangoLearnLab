f1e2fcdd30c236da24b04cb8bad171ef1fbabe82
Alazar Zemene <149241148+alazem@users.noreply.github.com>
2025-02-27 01:43:44 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,20 +2,20 @@ from relationship_app.models import Book, Author, Library, Librarian
 
 def book_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)  # Changed to use .filter()
     for book in books:
         print(f"Title: {book.title} by Author: {author.name}")
 
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    books = library.books.all()  # Fixed missing parentheses
+    books = library.books.all()
     for book in books:
         print(f"Title: {book.title}, Library: {library.name}")
 
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    librarians = library.librarians.all()  # Fixed incorrect assignment
-    for person in librarians:
+    librarian = Librarian.objects.filter(library=library)  # Using filter() for retrieval
+    for person in librarian:
         print(f"Librarian: {person.name}, Library: {library.name}")
 
 if __name__ == '__main__':
